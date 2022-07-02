165220211ae2180a994b34906cdced381a839723
Diane2617 <100887742+Diane2617@users.noreply.github.com>
2022-07-02 12:57:50 -0400

    update

--- WebMealPlanner.py
+++ WebMealPlanner.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def WelcomeGreeting(env,respond):
+def WelcomeGreeting():
     return f'''<h1>Welcome to the Meal Planner!<h1>
     <p>I can take the headache out of deciding what to cook. 
 I will give a a protein, and a method of preparation, with two sides.<p>
