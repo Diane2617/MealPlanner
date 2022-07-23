f491b0f0dd1ba5da72e2f50163a4b0d884090e1a
Diane2617 <100887742+Diane2617@users.noreply.github.com>
2022-07-23 00:12:17 -0400

    Creation of standard meal page

--- WebMealPlanner.py
+++ WebMealPlanner.py
@@ -10,31 +10,29 @@ from MealPlanner import Meal, meal_generator
 app = Flask(__name__)
 
 @app.route('/')
-def InitialGreeting():
-    return  f'''
-    
-  <form action="/home" method="POST">
-    <h3>Hi, what's your name?</h3>
-    <p>
-      <label for="Meal">Your Name:</label>
-      <input type="text" name="Name"/>
-    </p>
-    <input type="submit"/> 
-  </form>
-    '''
-
-
 @app.route('/home', methods=["GET", "POST"])
 def WelcomeGreeting():
-    return f'''<h1>Welcome {request.form["Name"]} to the Meal Planner!<h1>
+    return f'''<h1>Welcome to the Meal Planner!<h1>
     <p>I can take the headache out of deciding what to cook. You may choose to have
-    a standard meal which is a protein, a method of preparation, two sidese and a vegetable. 
-    Alternatively, you may choose how many sides and vegetable you wish to have with your meal.<p>
+    a standard meal which is a protein, a method of preparation, two sides and a vegetable. 
+    Alternatively, you may choose how many sides and vegetable you wish to have with your meal.</p>
+    
+    <p><a href="standard">Standard Meal</a></p>
 
-<form action="/meal" method="POST">
+    <p><a href="customMeal">Custom Meal</a></p>
+
+'''
+@app.route('/custom')
+def customMeal():
+  pass
+
+@app.route('/standard')
+def standard():
+  return f'''
+  <form action="/meal" method="POST">
     <h3>Input some information to help me get you started</h3>
     <p>
-      <label for="Meal">Your Name:</label>
+      <label for="Name">Your name:</label>
       <input type="text" name="Name"/>
     </p>
     <p>
@@ -43,23 +41,29 @@ def WelcomeGreeting():
     </p>
 
     <input type="submit"/> 
-</form>
-'''
-
+  </form>
+  '''
 
 @app.route('/meal', methods=["GET", "POST"])
 def mealshow():
-    mealdisplay = meal_generator(int(request.form["NumDays"]))
-    return f'''<p> Hi {request.form["Name"]}
+#  standardMeal = request.form["Standard"]
+
+#  {% if standardMeal == "Yes"}
+  mealdisplay = meal_generator(int(request.form["NumDays"]), 2, 1)
+  return f'''<p> Hi {request.form["Name"]} your meals are:
     <ul>
     {"".join([f"<li>{meal}</li>" for meal in mealdisplay])}
-  
-   
-</ul> </p>'''
+    </ul> </p> '''
+#  {% else %}
+#    mealdisplay = meal_generator(int(request.form["NumDays"]), int(request.form[2, 1)
+#      return f'''<p> Hi {request.form["Name"]}
+#      <ul>
+#      {"".join([f"<li>{meal}</li>" for meal in mealdisplay])}
+#      </ul> </p> '''
 
-@app.route('/custom')
-def customMeal():
-  pass
+#  {% endif %}  
+   
+
 
 if __name__ == '__main__':
     app.run(threaded=True)
