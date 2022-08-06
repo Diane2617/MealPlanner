60ce0cc47c5bd6498437571d2c2a84dd26b96ba4
Diane2617 <100887742+Diane2617@users.noreply.github.com>
2022-08-06 12:32:19 -0400

    Updates to templates and main application

--- WebMealPlanner.py
+++ WebMealPlanner.py
@@ -1,7 +1,7 @@
 #import Flask class
 from fileinput import filename
 from tokenize import Name
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 
 # import function from mealplanner
@@ -14,65 +14,33 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home', methods=["GET", "POST"])
 def WelcomeGreeting():
-    return f'''
-    <h1>Welcome to the Meal Planner!<h1>
-    <p>I can take the headache out of deciding what to cook. You may choose to have
-    a standard meal which is a protein, a method of preparation, two sides and a vegetable. 
-    Alternatively, you may choose how many sides and vegetable you wish to have with your meal.</p>
+    return render_template('home.html')
+
     
-    <p><a href="standard">Standard Meal</a></p>
+#      f'''
+#     <h1>Welcome to the Meal Planner!<h1>
+#     <p>I can take the headache out of deciding what to cook. You may choose to have
+#     a standard meal which is a protein, a method of preparation, two sides and a vegetable. 
+#     Alternatively, you may choose how many sides and vegetable you wish to have with your meal.</p>
+    
+#     <p><a href="standard">Standard Meal</a></p>
 
-    <p><a href="custom">Custom Meal</a></p>
+#     <p><a href="custom">Custom Meal</a></p>
 
-'''
+# '''
 @app.route('/custom')
 def customMeal():
-  return f'''
-  <form action="/meal" method="POST">
-    <h3 style = "color: rgba(36, 140, 238, 0.5);
-    font-family: Cambria;
-    font-weight: 100;
-    font-size: 22px;">Input some information to help me create your custom meal.</h3>
-    <p>
-      <label for="Name">Your name:</label>
-      <input type="text" name="Name"/>
-    </p>
-    <p>
-      <label for="NumDays">Number of days you would like the meal for:</label>
-      <input type="number" name="NumDays"/>
-    </p>
-
-    <p>
-      <label for="CustomSides">Number of sides:</label>
-      <input type="number" name="CustomSides"/>
-    </p>
-
-    <p>
-      <label for="CustomVege">Number of veges:</label>
-      <input type="number" name="CustomVege"/>
-    </p>
-    <input type="submit"/> 
-</form>
-  '''
+  return render_template('meal_info.html', choice='custom')
 
 @app.route('/standard')
 def standard():
-  return f'''
-  <form action="/meal" method="POST">
-    <h3>Input some information to help me get you started</h3>
-    <p>
-      <label for="Name">Your name:</label>
-      <input type="text" name="Name"/>
-    </p>
-    <p>
-      <label for="NumDays">Number of days you would like the meal for:</label>
-      <input type="number" name="NumDays"/>
-    </p>
+  return render_template('meal_info.html', choice='standard')
 
-    <input type="submit"/> 
-  </form>
-  '''
+@app.route('/yourbuild')
+def yourMeal():
+  return render_template('meal_info.html')
 
+  
 @app.route('/meal', methods=["GET", "POST"])
 def mealshow():
   print(request.form)
---
+++ next steps database.md
@@ -0,0 +1,25 @@
+ Careful how storing passwords, and usernames, emails. 
+ Administrator user name and password so that I can access database
+ two 
+ 1. SQL -  older, more structured way for storing and access data
+ 2. non SQL - not as structured,
+
+ make distinction between user info and meal info
+
+ approach 1 - using tables
+
+ approach 2 - a document for each user
+
+ find python connector library for database
+
+ {% if users %}
+<ul>
+</ul>
+{% endif %}
+{% if kenny.sick %}
+    Kenny is sick.
+{% elif kenny.dead %}
+    You killed Kenny!  You bastard!!!
+{% else %}
+    Kenny looks okay --- so far
+{% endif %}
